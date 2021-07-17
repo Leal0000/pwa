4a1f7ed1041ad8b879658a81733a1aa5e1da4b66
Leal0000 <arielleal2202@gmail.com>
2021-07-17 14:39:34 -0500

    archivos angular

---
+++ pwa-app/src/assets/js/main.js
@@ -0,0 +1,23 @@
+var typed = new Typed('#typed',{
+                   strings:["la realidad en datos.","datos en información.", "información en soluciones.", "soluciones en negocio."],
+                   backSpeed: 80,
+                   typeSpeed: 40,
+                   loop: true,
+                   shuffle: false
+                 });
+
+
+
+                 (function(h,o,t,j,a,r){
+                  h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+                  h._hjSettings={hjid:1826042,hjsv:6};
+                  a=o.getElementsByTagName('head')[0];
+                  r=o.createElement('script');r.async=1;
+                  r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+                  a.appendChild(r);
+              })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
+              
+              
+              $(function () {
+            $('[data-toggle="popover"]').popover()
+          })
